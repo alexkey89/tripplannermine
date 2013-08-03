f7a1307ed93000be829ebedb499c631f6c4ed66e
alexkey89 <alexkey89@gmail.com>
2013-08-03 21:29:44 +0300

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,57 @@
+
+
+<?php
+/*
+loads the form field data into variables.
+*/
+
+$Region_Name_nic = $_POST['region_name_nic'] ;
+$Region_Name_lar = $_POST['region_name_lar'] ;
+$Region_Name_lim = $_POST['region_name_lim'] ;
+$Region_Name_paph = $_POST['region_name_paph'] ;
+$Region_Name_fam = $_POST['region_name_fam'] ;
+
+
+$Level_Name_mon = $_POST['level_name_mon'] ;
+$Level_Name_meu = $_POST['level_name_meu'] ;
+$Level_Name_anc = $_POST['level_name_anc'] ;
+$Level_Name_sight = $_POST['level_name_sight'] ;
+$Level_Name_gol = $_POST['level_name_gol'] ;
+$Level_Name_foot = $_POST['level_name_foot'] ;
+$Level_Name_pub = $_POST['level_name_pub'] ;
+$Level_Name_club = $_POST['level_name_club'] ;
+$Level_Name_mount = $_POST['level_name_mount'] ;
+$Level_Name_beach = $_POST['level_name_beach'] ;
+
+/*
+echo $Level_Name_mon;
+echo $Level_Name_meu;
+echo $Level_Name_anc;
+echo $Level_Name_sight;
+echo $Level_Name_gol;
+echo $Level_Name_foot;
+echo $Level_Name_pub;
+echo $Level_Name_club;
+echo $Level_Name_mount;
+echo $Level_Name_beach;
+*/
+
+
+
+
+
+/*
+$visitor_surname = $_GET['txtSurname'] ;
+$email_address = $_POST['txtEmail'] ;
+$street = $_POST['txtStreet'] ;
+$city = $_POST['txtCity'] ;
+$postal_code = $_POST['txtPostalCode'] ;
+$country = $_POST['txtCountry'] ;
+$company = $_POST['txtCompany'] ;
+$phone = $_POST['txtPhone'] ;
+$fax = $_POST['txtFax'] ;
+$mobile = $_POST['txtMobile'] ;
+$message = $_POST['txtMessage'] ;
+*/
+?>
+
