82a4b388ddb4ba4bf0e04f97e244a570040fde0c
alexkey89 <alexkey89@gmail.com>
2013-08-11 14:46:45 +0300

    Update variables.php

--- variables.php
+++ variables.php
@@ -2,16 +2,24 @@
 
 <?php
 /*
-loads the form field data into variables.
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
 */
+$Region_set = 0;
+$Location_set = 0;
 
+if(isset($_POST['region_name_nic']) OR isset($_POST['region_name_lar']) OR isset($_POST['region_name_lim']) OR isset($_POST['region_name_paph']) OR isset($_POST['region_name_fam'])) 
+{
 $Region_Name_nic = $_POST['region_name_nic'] ;
 $Region_Name_lar = $_POST['region_name_lar'] ;
 $Region_Name_lim = $_POST['region_name_lim'] ;
 $Region_Name_paph = $_POST['region_name_paph'] ;
 $Region_Name_fam = $_POST['region_name_fam'] ;
+$Region_set = 1;
+}
 
-
+if(isset($_POST['level_name_mon']) OR isset($_POST['level_name_meu']) OR isset($_POST['level_name_anc']) OR isset($_POST['level_name_sight']) OR isset($_POST['level_name_gol']) OR isset($_POST['level_name_foot']) OR isset($_POST['level_name_pub']) OR isset($_POST['level_name_club']) OR isset($_POST['level_name_mount']) OR isset($_POST['level_name_beach']) ) 
+{
 $Level_Name_mon = $_POST['level_name_mon'] ;
 $Level_Name_meu = $_POST['level_name_meu'] ;
 $Level_Name_anc = $_POST['level_name_anc'] ;
@@ -22,6 +30,10 @@ $Level_Name_pub = $_POST['level_name_pub'] ;
 $Level_Name_club = $_POST['level_name_club'] ;
 $Level_Name_mount = $_POST['level_name_mount'] ;
 $Level_Name_beach = $_POST['level_name_beach'] ;
+$Location_set = 1;
+}
+
+
 
 /*
 echo $Level_Name_mon;
@@ -36,22 +48,5 @@ echo $Level_Name_mount;
 echo $Level_Name_beach;
 */
 
-
-
-
-
-/*
-$visitor_surname = $_GET['txtSurname'] ;
-$email_address = $_POST['txtEmail'] ;
-$street = $_POST['txtStreet'] ;
-$city = $_POST['txtCity'] ;
-$postal_code = $_POST['txtPostalCode'] ;
-$country = $_POST['txtCountry'] ;
-$company = $_POST['txtCompany'] ;
-$phone = $_POST['txtPhone'] ;
-$fax = $_POST['txtFax'] ;
-$mobile = $_POST['txtMobile'] ;
-$message = $_POST['txtMessage'] ;
-*/
 ?>
 
